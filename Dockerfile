c7b0ed0e79aaa00134ec161b03493926cfa53a18
Denzen Ddz <denzenxxx@Denzens-MacBook-Air.local>
2025-10-01 19:57:49 +0900

    fixed ishappy code

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM python:3.12-slim
-COPY app.py /app.py
-CMD ["python", "/app.py"]
+
+COPY ishappy.py /app/ishappy.py
+CMD ["python", "/app/ishappy.py"]
+
--- ishappy.py
+++ ishappy.py
@@ -3,26 +3,21 @@ import os
 
 def isHappy(n):
     happyNumbers = []
-    for i in range(int(math.log10(n)) + 1):
-        number = int(n / pow(10, int(math.log10(n)) - i))
-        nextNumber = number
-        if nextNumber > 10:
-            number = nextNumber % 10
-        happyNumbers.append(number)
-
-    result = [x**2 for x in happyNumbers]
-    planB = sum(result)
-    return planB == 1
+    while n != 1 and n not in happyNumbers:
+        happyNumbers.append(n)
+        n = sum(int(x)**2 for x in str(n))
+    return n == 1
 
 if __name__ == "__main__":
     sample0_output = isHappy(19)
     sample1_output = isHappy(2)
 
-    os.makedirs("/app/bind_mount", exist_ok=True)
+    os.makedirs("bind_mount", exist_ok=True)
 
-    with open("/app/bind_mount/output.txt", "w") as f:
+    with open("bind_mount/output.txt", "w") as f:
         f.write(f"19: {sample0_output}\n")
         f.write(f"2: {sample1_output}\n")
 
-    print("Results saved to /app/bind_mount/output.txt")
+    print("Results saved to bind_mount/output.txt")
+
 
